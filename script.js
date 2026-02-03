aa5aeb727ff347ded698da0789e9e4fd4a580ce6
sethuram333 <sethurammohan.m@gmail.com>
2026-02-03 18:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 //your JS code here. If required.
 
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-// remove leading articles
+// your JS code here. If required.
+
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+// function to remove articles
 function stripArticle(name) {
   return name.replace(/^(a |an |the )/i, '').trim();
 }
 
+// sort bands
 const sortedBands = bands.sort((a, b) => {
   return stripArticle(a).localeCompare(stripArticle(b));
 });
 
-console.log(sortedBands);
+// add to DOM
+const ul = document.getElementById('band');
+
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
