3aff11303593b8c65139626a922d0591704f65d3
sethuram333 <sethurammohan.m@gmail.com>
2026-02-03 18:19:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+
+// remove leading articles
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => {
+  return stripArticle(a).localeCompare(stripArticle(b));
+});
+
+console.log(sortedBands);
